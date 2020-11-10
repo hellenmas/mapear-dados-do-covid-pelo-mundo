539a671589773fdc56e4d804bc8c7b01f706656d
hellenmas <73563463+hellenmas@users.noreply.github.com>
2020-11-10 18:43:53 -0300

    Add files via upload

--- src/containers/style.js
+++ src/containers/style.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components'
+import { CardContent, Typography } from '../../../components'
 
-export const ContainerStyled = styled.div`
-  padding: 20px;
-  margin: 0 auto;
-  max-width: 1024px;
+export const LabelStyled = styled(Typography)`
+  font-weight: 500;
+  font-size: 1.5rem;
 `
 
-export const ContainerDev = styled.h2`
-  color: #ffffff;
-  background: #000000;
-  text-align: center;
+export const ValueStyled = styled(Typography)`
+  font-weight: 400;
+  font-size: 2.5rem;
+`
+
+export const CardContentStyled = styled(CardContent)`
+  border-left: 8px solid ${({ color }) => color || '#5d78ff'};
+`
+
+export const CardPanelContentStyled = styled(CardContent)`
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
+`
+
+export const ItemStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-width: 150px;
 `
