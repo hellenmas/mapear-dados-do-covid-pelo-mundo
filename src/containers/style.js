fd40877e28a9467104b1ff691e429d43e7c1e345
hellenmas <73563463+hellenmas@users.noreply.github.com>
2020-11-10 18:43:17 -0300

    Create style.js

---
+++ src/containers/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ContainerStyled = styled.div`
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 1024px;
+`
+
+export const ContainerDev = styled.h2`
+  color: #ffffff;
+  background: #000000;
+  text-align: center;
+`
